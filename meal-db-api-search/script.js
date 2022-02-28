b198c8dff7d38b3b38792cead699a807b925a10b
Nayim hossen <nayimh2015@gmail.com>
2022-02-28 21:36:59 +0600

    mealdb-api error-handle

--- meal-db-api-search/script.js
+++ meal-db-api-search/script.js
@@ -1,22 +1,31 @@
 const searchFood = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    searchField.value = '';
 
-    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displaySearchResult(data.meals))
+    if (searchText == "") {
+        document.getElementById('waring-custom').style.display = "block";
+    } else {
+        searchField.value = '';
+
+        const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displaySearchResult(data.meals))
+    }
 }
 
 const displaySearchResult = (meals) => {
     const searchResult = document.getElementById('search-result');
+    searchResult.textContent = '';
+
+    if (meals.length == 0) {
+        document.getElementById('waring-custom').style.display = "block";
+    }
     meals.forEach(meal => {
-        console.log(meal);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-        <div class="card h-100">
+        <div onclick="loadMealDetail(${meal.idMeal})" class="card h-100">
             <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${meal.strMeal}</h5>
@@ -26,4 +35,34 @@ const displaySearchResult = (meals) => {
         `;
         searchResult.appendChild(div);
     })
+}
+
+const loadMealDetail = (mealId) => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMealDetail(data.meals[0]))
+}
+
+const displayMealDetail = (meal) => {
+    const mealDetails = document.getElementById('meal-details');
+    mealDetails.textContent = '';
+    const div = document.createElement('div');
+    div.classList.add('card');
+
+    div.innerHTML = `
+    <div class="row g-0">
+        <div class="col-md-4">
+            <img height="50" src="${meal.strMealThumb}" class="img-fluid rounded-start" alt="...">
+        </div>
+        <div class="col-md-8">
+            <div class="card-body">
+                <h5 class="card-title">${meal.strMeal}</h5>
+                <p class="card-text">${meal.strInstructions.slice(0, 150)}.</p>
+                <a href="${meal.strYoutube}" class="btn btn-primary">go youtube</a>
+            </div>
+        </div>
+    </div>
+    `;
+    mealDetails.appendChild(div);
 }
